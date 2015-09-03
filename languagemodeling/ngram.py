1b1c2094d27e37cfd9170697979eac3524b9b301
marianaw <mariana.09.w@gmail.com>
2015-09-03 21:34:27 +0100

    count and cond_prob

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # https://docs.python.org/3/library/collections.html
 from collections import defaultdict
 
@@ -14,10 +15,12 @@ class NGram(object):
         self.counts = counts = defaultdict(int)
 
         for sent in sents:
+            sent = ['<s>'] + sent + ['</s>']
             for i in range(len(sent) - n + 1):
                 ngram = tuple(sent[i: i + n])
                 counts[ngram] += 1
                 counts[ngram[:-1]] += 1
+
 
     def prob(self, token, prev_tokens=None):
         n = self.n
@@ -27,3 +30,48 @@ class NGram(object):
 
         tokens = prev_tokens + [token]
         return float(self.counts[tuple(tokens)]) / self.counts[tuple(prev_tokens)]
+    
+    
+    def count(self, tokens):
+        """Count for an n-gram or (n-1)-gram.
+ 
+        tokens -- the n-gram or (n-1)-gram tuple.
+        """
+        if tokens in self.counts.keys():
+            #Hardcodeo espantoso (no entiendo muy bien cómo funciona el <s>):
+            if tokens == ():
+                return self.counts[tokens]-2
+            if self.n == 1 and tokens == ('<s>',):
+                return 0
+            
+            return self.counts[tokens]
+        else:
+            return 0
+ 
+ 
+    def cond_prob(self, token, prev_tokens=None):
+        """Conditional probability of a token.
+ 
+        token -- the token.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+        if prev_tokens is None:
+            prev_tokens = tuple()
+        else:
+            prev_tokens = tuple(prev_tokens)
+        #import ipdb; ipdb.set_trace()
+        return float(self.count(prev_tokens + (token,)))/float(self.count(prev_tokens))
+ 
+ 
+    def sent_prob(self, sent):
+        """Probability of a sentence. Warning: subject to underflow problems.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+ 
+ 
+    def sent_log_prob(self, sent):
+        """Log-probability of a sentence.
+ 
+        sent -- the sentence as a list of tokens.
+        """
