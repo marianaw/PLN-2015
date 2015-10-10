eadb7ca60ce2c9b702df155831b45948beb2327c
marianaw <mariana.09.w@gmail.com>
2015-10-10 17:57:33 -0300

    Script generate.py to load a model from file and generate sentences.

--- languagemodeling/scripts/generate.py
+++ languagemodeling/scripts/generate.py
@@ -1,21 +1,19 @@
-from optparse import OptionParser
-import sys
+"""Generate sentences.
 
-sys.path.append('~/Documents/Materias_de_posgrado/2015/PLN/PLN-2015/languagemodeling/')
+Usage:
+  generate.py -n <n> -i <file>
+  generate.py -h | --help
+
+Options:
+  -n <n>        Order of the model.
+  -i <file>     Input model file.
+  -h --help     Show this screen.
+"""
+from docopt import docopt
+from pickle import load
+
+#sys.path.append('~/Documents/Materias_de_posgrado/2015/PLN/PLN-2015/languagemodeling/')
 from languagemodeling.ngram import *
-
-
-def read_model(f):
-    with open(f, 'r') as fl:
-        sents = []
-        for line in fl:
-            sents.append(line.strip('\n').split())
-        n = int(sents[0][0])
-        sents = sents[1:]
-        ng = NGram(n, sents)
-        fl.close()
-    return ng
-
 
 
 def generate_sentences(model, n):
@@ -26,16 +24,15 @@ def generate_sentences(model, n):
 
 
 if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option("-i", dest="filename", help="File containing language model.")
-    parser.add_option("-n", dest="sentsnum", help="The number of sentences to be generated.")
-    opt, args = parser.parse_args()
+    opts = docopt(__doc__)
     
-    n = int(opt.sentsnum)
-    f = opt.filename
-    model = read_model(f)
+    n = int(opts['-n'])
+    with open(opts['-i'], 'rb') as f:
+        f.seek(0)
+        model = load(f)
     
-    sentences = generate_sentences(model, n)
-    for sentence in sentences:
-        print(sentence)
+        sentences = generate_sentences(model, n)
+        for sentence in sentences:
+            print(' '.join(sentence))
+            print('----------*----------')
     
