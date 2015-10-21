9e565b8e094a89025532e725409b4552489f6080
marianaw <mariana.09.w@gmail.com>
2015-10-21 01:15:45 -0300

    Ejercicio 2.

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,3 +1,25 @@
+from functools import reduce
+from collections import OrderedDict
+
+
+def merge(d, sd):
+    skey= list(sd.keys())[0]
+    for key in d.keys():
+        if key == skey:
+            d1 = d[key]
+            d2 = sd[skey]
+            d2_key = list(d2.keys())[0]
+            for d1_key in d1.keys():
+                if d2_key == d1_key:
+                    d1.update({d1_key: d1[d1_key] + d2[d2_key]})
+                    d.update({key: d1})
+                    return d
+            d1.update(d2)
+            d.update({key:d1})
+            return d
+    d.update(sd)
+    return d
+
 
 
 class BaselineTagger:
@@ -6,7 +28,22 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+        tagged = []
+        for t in tagged_sents:
+            tagged += t
+        l = list(map(lambda x: {x[0]: { x[1]: 1}}, tagged))
+        self.tag_counts = reduce(merge, l)
+        aux = list(self.tag_counts.values())
+        counts = {}
+        for item in aux:
+            for key in item:
+                if key in counts.keys():
+                    vale = counts[key]
+                    counts.update({key: vale + item[key]})
+                else:
+                    counts.update(item)
+        li = sorted(counts.items(), key=lambda x:x[1], reverse=True)
+        self.default_tag = li[0][0]
 
     def tag(self, sent):
         """Tag a sentence.
@@ -20,11 +57,32 @@ class BaselineTagger:
 
         w -- the word.
         """
-        return 'nc'
+        if not self.unknown(w):
+            tags = self.tag_counts[w]
+            ordered_tags = OrderedDict(tags)
+            return max(ordered_tags)
+        else:
+            return self.default_tag
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
         w -- the word.
         """
-        return True
+        return not w in self.tag_counts.keys()
+
+#if __name__ == '__main__':
+    #from functools import reduce
+    #t = [('el', ('D', 1)),
+    #('gato', ('N', 1)),
+    #('come', ('V', 1)),
+    #('pescado', ('N', 1)),
+    #('.', ('P', 1)),
+    #('el', ('V', 1)),
+    #('la', ('V', 1)),
+    #('el', ('V', 1)),
+    #('el', ('V', 1))]
+
+    #d = {}
+    #reduce(merge(d), t)
+    #print(d)
