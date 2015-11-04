47a462f2c58541b401bcccf5f2a90510c22881f7
marianaw <mariana.09.w@gmail.com>
2015-11-03 22:54:59 -0300

    HMLMM con un par de bugs.

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -1,4 +1,5 @@
 from math import log
+from collections import defaultdict
 
 log2 = lambda x: log(x, 2)
 
@@ -163,3 +164,81 @@ class ViterbiTagger:
         last_dict = d[len(sent)]
         max_prob = max(last_dict.values(), key=lambda x: x[0])
         return max_prob[1]
+
+
+
+class MLHMM(HMM):
+ 
+    def __init__(self, n, tagged_sents, addone=True):
+        """
+        n -- order of the model.
+        tagged_sents -- training sentences, each one being a list of pairs.
+        addone -- whether to use addone smoothing (default: True).
+        """
+        self.n = n
+        self.addone = addone
+        self.start_symbol = '<s>'
+        self.counts = counts = defaultdict(int)
+        tags = []
+        self.words = words = set()
+        start_phrase = [self.start_symbol] * (n - 1)
+        for sent in tagged_sents:
+            tag_seq = [t for w, t in sent]
+            tags += tag_seq
+            tag_seq = start_phrase + tag_seq + ['</s>']
+            for i in range(len(tag_seq) - n + 1):
+                ngram = tuple(tag_seq[i: i + n])
+                counts[ngram] += 1
+                counts[ngram[:-1]] += 1
+            tags += tag_seq
+            words = words.union(set([w for w, t in sent]))
+        tagged = [t for  sent in tagged_sents for t in sent]
+        self.tag_word_pair_count = dict((x, tagged.count(x)) for x in set(tagged))
+        self.every_tag_count = dict((x, tags.count(x)) for x in set(tags))
+        self.voc_size = len(words)
+ 
+    def tcount(self, tokens):
+        """Count for an n-gram or (n-1)-gram of tags.
+ 
+        tokens -- the n-gram or (n-1)-gram tuple of tags.
+        """
+        try:
+            return self.counts[tokens]
+        except KeyError:
+            return 0
+ 
+    def unknown(self, w):
+        """Check if a word is unknown for the model.
+ 
+        w -- the word.
+        """
+        return w not in self.words
+    
+    def trans_prob(self, tag, prev_tags):
+        """Transition probability for tags ngram.
+
+        tag -- the tag.
+        prev_tags -- the previous tags.
+        """
+        if prev_tags is None:
+            prev_tags = ()
+        num = float(self.tcount(tuple(prev_tags) + (tag,)))
+        den = float(self.tcount(tuple(prev_tags)))
+        if self.addone:
+            num = num + 1
+            den = den + self.voc_size
+            return num/den
+        if den > 0:
+            return num/den
+        
+    def out_prob(self, word, tag):
+        """Probability of a word given a tag.
+
+        word -- the word.
+        tag -- the tag.
+        """
+        if self.unknown(word):
+            return 1/float(self.voc_size)
+        num = float(self.tag_word_pair_count[(word, tag)])
+        den = float(self.every_tag_count[tag])
+        return num/den
